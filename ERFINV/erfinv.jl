5fed4ca1a067cc64cd8f2a30e72e833064007619
fockl <j4xn3gvbxqtfdnxg@gmail.com>
2020-10-03 10:58:46 +0900

    [update] update jl & cmakelists for fast benchmark

--- ERFINV/erfinv.jl
+++ ERFINV/erfinv.jl
@@ -4,12 +4,10 @@ println(erfinv(0.5))
 println(erfinv(0.9))
 println(erfinv(0.99))
 
-N = 1E8
-
-function main()
+function main(N)
   for i in 0:N-1
     erfinv(i/N)
   end
 end
 
-@time main()
+@time main(1E8)
